2de6d6c23d37f9f524f86ac9aed15ebae6b99b7f
EmuTheEmo <jaysgames.jsg@gmail.com>
2022-04-04 16:14:15 +0100

    Added Advanced Item

--- src/main/java/net/jay/testingmod/item/ModItems.java
+++ src/main/java/net/jay/testingmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.jay.testingmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.jay.testingmod.TestingMod;
+import net.jay.testingmod.item.custom.DowsingRodItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -17,6 +18,9 @@ public class ModItems {
 
     public static final Item RAW_MYTHRIL = registerItem("raw_mythril",
             new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+
+    public static final Item DOWSING_ROD = registerItem("dowsing_rod",
+            new DowsingRodItem(new FabricItemSettings().group(ModItemGroup.MYTHRIL).maxDamage(16)));
 
 
     private static Item registerItem(String name, Item item) {
